c1627fae0b5ca9974df74c361f5aa990bcb013bf
CHHORN Seyha <senjihachiro@gmail.com>
2026-05-29 11:38:16 +0700

    feat: added full CRUD opeartion

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
@@ -22,6 +24,7 @@ class CategoryController extends Controller
     public function create()
     {
         //
+        return view('category.create');
     }
 
     /**
@@ -30,6 +33,17 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         //
+         $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required|max:255',
+        ]);
+        // Create The Category
+        $category = new Category;
+        $category->name = $request->name;
+        $category->description = $request->description;
+        $category->save();
+        Session::flash('category_create','Category is created.');
+        return redirect('/category/create');
     }
 
     /**
@@ -38,6 +52,8 @@ class CategoryController extends Controller
     public function show(string $id)
     {
         //
+        $category = Category::find($id);
+    return view('category.show')->with('category',$category);
     }
 
     /**
@@ -46,6 +62,9 @@ class CategoryController extends Controller
     public function edit(string $id)
     {
         //
+          $category = Category::find($id);
+        return view('category.edit')->with('category', $category);
+        
     }
 
     /**
@@ -54,6 +73,25 @@ class CategoryController extends Controller
     public function update(Request $request, string $id)
     {
         //
+
+           $validator = Validator::make($request->all(), [
+      'name' => 'required|max:20|min:3',
+            'description' => 'required|max:20|min:3',
+    ]);
+    if ($validator->fails()) {
+      return redirect('category/' . $id . '/edit')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    // Create The Category
+    $category = Category::find($id);
+    $category->name = $request->Input('name');
+        $category->description = $request->Input('description');
+    $category->save();
+    Session::flash('category_update','Category is updated.');
+    return redirect('category/' . $id . '/edit');
+
+        
     }
 
     /**
@@ -62,5 +100,9 @@ class CategoryController extends Controller
     public function destroy(string $id)
     {
         //
+         $category = Category::find($id);
+        $category->delete();
+        Session::flash('category_delete','Category is deleted.');
+        return redirect('category');
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Session;
 
 class Category extends Model
 {
+
 
 
    protected $table = 'categories';
